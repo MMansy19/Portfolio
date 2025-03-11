b815b849c5fc55198fec8b10f48efba3302007e1
Mahmoud Mansy <mahmoudmohamedk8200949@gmail.com>
2025-03-11 17:29:26 +0200

    fix bugs

--- assets/scripts/skillsDynamically.js
+++ assets/scripts/skillsDynamically.js
@@ -6,23 +6,22 @@ const generateStars = (starCount) => {
   );
   return starsArray.join(" ");
 };
+
 // Add Skill Dynamically
 const skillsArray = [
   // Frontend Skills
-  //
   {
-    name: "HTML",
-    experience: "2021",
+    name: "React.js",
+    experience: "2023",
     stars: "5",
     type: "frontend",
   },
   {
-    name: "CSS",
-    experience: "2021",
-    stars: "5",
+    name: "Next.js",
+    experience: "2023",
+    stars: "4",
     type: "frontend",
   },
-
   {
     name: "JavaScript",
     experience: "2022",
@@ -36,28 +35,9 @@ const skillsArray = [
     type: "frontend",
   },
   {
-    name: "React.js",
-    experience: "2023",
-    stars: "4",
-    type: "frontend",
-  },
-  {
-    name: "Next.js",
-    experience: "2023",
-    stars: "3",
-    type: "frontend",
-  },
-
-  {
     name: "Tailwind CSS",
     experience: "2023",
     stars: "5",
-    type: "frontend",
-  },
-  {
-    name: "SASS",
-    experience: "2022",
-    stars: "4",
     type: "frontend",
   },
   {
@@ -67,40 +47,16 @@ const skillsArray = [
     type: "frontend",
   },
   {
-    name: "Material UI",
-    experience: "2023",
-    stars: "5",
-    type: "frontend",
-  },
-
-  {
     name: "UX/UI Design",
     experience: "2023",
     stars: "5",
     type: "frontend",
   },
-  {
-    name: "RESTful APIs",
-    experience: "2023",
-    stars: "3",
-    type: "frontend",
-  },
+
   // Backend Skills
   {
-    name: "Python",
-    experience: "2021",
-    stars: "5",
-    type: "backend",
-  },
-  // {
-  //   name: "GraphQL",
-  //   experience: "2022",
-  //   stars: "3",
-  //   type: "backend",
-  // },
-  {
-    name: "Firebase",
-    experience: "2023",
+    name: "Node.js",
+    experience: "2022",
     stars: "3",
     type: "backend",
   },
@@ -111,17 +67,12 @@ const skillsArray = [
     type: "backend",
   },
   {
-    name: "PostgreSQL",
-    experience: "2022",
-    stars: "3",
+    name: "Firebase",
+    experience: "2023",
+    stars: "4",
     type: "backend",
   },
-  {
-    name: "Node.js",
-    experience: "2022",
-    stars: "3",
-    type: "backend",
-  },
+
   // General Skills
   {
     name: "Git/GitHub",
@@ -130,7 +81,7 @@ const skillsArray = [
     type: "general",
   },
   {
-    name: "Vite",
+    name: "Docker",
     experience: "2023",
     stars: "4",
     type: "general",
@@ -138,49 +89,31 @@ const skillsArray = [
   {
     name: "CI/CD",
     experience: "2023",
-    stars: "3",
+    stars: "4",
     type: "general",
   },
 
-  // // Soft Skills
+  // Soft Skills
   {
     name: "Problem-solving",
     experience: "2022",
-    stars: "4",
+    stars: "5",
     type: "soft",
   },
   {
     name: "Teamwork",
     experience: "2023",
-    stars: "4",
+    stars: "5",
+    type: "soft",
+  },
+  {
+    name: "Communication",
+    experience: "2023",
+    stars: "5",
     type: "soft",
   },
   {
     name: "Leadership",
-    experience: "2023",
-    stars: "4",
-    type: "soft",
-  },
-  {
-    name: "Flexibility",
-    experience: "2023",
-    stars: "4",
-    type: "soft",
-  },
-  {
-    name: "Self-motivation",
-    experience: "2023",
-    stars: "4",
-    type: "soft",
-  },
-  {
-    name: "communication",
-    experience: "2023",
-    stars: "4",
-    type: "soft",
-  },
-  {
-    name: "Decision-making",
     experience: "2023",
     stars: "4",
     type: "soft",
@@ -192,13 +125,11 @@ const currentYear = new Date().getFullYear();
 
 const generateListItem = (skillItem) => {
   const starsHtml = generateStars(parseInt(skillItem.stars));
-  return `<li class="skills__item" data-type=${skillItem.type} data-name=${
-    skillItem.name
-  }>
+  return `<li class="skills__item" data-type=${skillItem.type} data-name=${skillItem.name
+    }>
     <h2 class="skills__title">${skillItem.name}</h2>
     <span class="skills__stars">${starsHtml}</span>
-    <br/><span class="skills__years">${
-      currentYear - skillItem.experience
+    <br/><span class="skills__years">${currentYear - skillItem.experience
     } years</span>
   </li>`;
 };
@@ -211,8 +142,6 @@ const unOrderList = `<ul class="skills__list">${skillItemsString}</ul>`;
 
 skillsElement.insertAdjacentHTML("beforeend", unOrderList);
 
-//======================================
-//----
 // Highlight Clicked Item
 const skillsItems = document.querySelectorAll(".skills__item");
 
